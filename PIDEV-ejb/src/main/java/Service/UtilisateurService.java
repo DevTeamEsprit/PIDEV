2c1de8a631a2d6d4a8144ed8120c73978fd0afa5
Elyes <Elyes@DESKTOP-QNI24BT>
2019-10-20 20:20:27 +0100

    create first service

---
+++ PIDEV-ejb/src/main/java/Service/UtilisateurService.java
@@ -0,0 +1,20 @@
+package Service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class UtilisateurService
+ */
+@Stateless
+@LocalBean
+public class UtilisateurService implements UtilisateurServiceLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public UtilisateurService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
