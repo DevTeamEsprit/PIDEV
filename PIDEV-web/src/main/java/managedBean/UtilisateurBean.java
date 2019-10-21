0f29bf42ce76fad90fc164b27cec91dd8297174f
Elyes <Elyes@DESKTOP-QNI24BT>
2019-10-21 20:52:10 +0100

    manged bean

---
+++ PIDEV-web/src/main/java/managedBean/UtilisateurBean.java
@@ -0,0 +1,12 @@
+package managedBean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class UtilisateurBean implements Serializable{
+
+}
