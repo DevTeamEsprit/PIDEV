1315fd8b54ca00127148a98bf0303d00ff50dd13
clydemino <clydevianney.omogntap@esprit.tn>
2019-10-23 08:23:54 +0000

    evalview

--- PIDEV-web/src/main/java/managedBean/EvaluationBean.java
+++ PIDEV-web/src/main/java/managedBean/EvaluationBean.java
@@ -1,15 +1,35 @@
 package managedBean;
 
+import java.util.Date;
+
 import javax.ejb.EJB;
 import javax.faces.bean.*;
 
 import Service.evaluation.EvaluationService;
+import entity.EvalType;
 
 @ManagedBean
 @SessionScoped
 public class EvaluationBean {
-
+       private Date date;
+       private EvalType evalType;
 	
+	public EvalType getEvalType() {
+		return evalType;
+	}
+
+	public void setEvalType(EvalType evalType) {
+		this.evalType = evalType;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 	@EJB
 	EvaluationService evaluationService;
 	
