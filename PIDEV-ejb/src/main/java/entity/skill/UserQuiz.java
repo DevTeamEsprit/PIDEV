55a39b2b9db859d82108a2257f549a11a6a124c5
eyagargah <eya.gargah@esprit.tn>
2019-10-26 19:09:10 +0100

    Update UserQuiz.java

--- PIDEV-ejb/src/main/java/entity/skill/UserQuiz.java
+++ PIDEV-ejb/src/main/java/entity/skill/UserQuiz.java
@@ -1,8 +1,16 @@
 package entity.skill;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-public class UserQuiz  {
-
+public class UserQuiz implements Serializable  {
+ 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 }
